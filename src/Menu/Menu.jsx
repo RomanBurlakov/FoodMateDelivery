dba45c7e14d46bfc63b448d1dcf5507bb288733a
RomanBurlakov <roma160398@yandex.ru>
2024-02-03 13:43:02 +0300

    fix positon

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -4,7 +4,14 @@ import FoodCard from '../FoodCard/FoodCard'
 import useAPIServices from '../service/APIService'
 import Spiner from '../assets/Spinner.svg'
 import Error from '../assets/error.jpg'
+import american from '../assets/american.jpg'
+import british from '../assets/british.jpg'
+import french from '../assets/french.jpg'
+import italian from '../assets/italian.jpg'
+import japanese from '../assets/japanese.jpg'
+import russian from '../assets/russian.jpg'
 
+const counrtyTabsFlags = [russian, japanese, american, italian, french, british];
 const counrtyTabs = ['russian', 'japanese', 'american', 'italian', 'french', 'british'];
 
 export default function Menu() {
@@ -37,9 +44,10 @@ export default function Menu() {
                     if (activeTab == i) {
                         className += ' active';
                     }
+                    const src = counrtyTabsFlags[counrtyTabs.findIndex((elem) => e === elem)];
                     return (
                         <div onClick={() => setActiveTab(i)} className={className} key={i}>
-                            <img src={"/src/assets/" + e + '.jpg'} alt={e} className='flag' />
+                            <img src={src} alt={e} className='flag' />
                         </div>
                     )
                 })}
